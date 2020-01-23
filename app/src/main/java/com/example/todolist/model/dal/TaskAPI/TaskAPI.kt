57ff95b8e25abe6e3639dbbe0a399d67f04e845c
Żaneta <zaneta.mielczarek5@wp.pl>
2020-01-23 21:06:21 +0100

    dodano wszystkie funkcje api dla tasków

---
+++ app/src/main/java/com/example/todolist/model/dal/TaskAPI/TaskAPI.kt
@@ -0,0 +1,171 @@
+package com.example.todolist.model.dal.TaskAPI
+
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import com.example.todolist.model.Task
+import com.google.gson.GsonBuilder
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class TaskAPI {
+    val BASE_URL = "http://192.168.0.105:8080/"
+
+    val retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(
+            GsonConverterFactory.create(
+                GsonBuilder()
+                    .setLenient()
+                    .create()
+            ))
+        .build()
+
+    val service = retrofit.create(TaskService::class.java)
+
+    fun getAllTasks(): MutableLiveData<List<Task>> {
+        val listOfAllTasks = MutableLiveData<List<Task>>()
+        val call = service.getAllTasks()
+
+        call.enqueue(object : Callback<List<Task>> {
+            override fun onResponse(call: Call<List<Task>>, response: Response<List<Task>>) {
+                if (response.isSuccessful) {
+                    listOfAllTasks.value = response.body()!!
+                }
+            }
+            override fun onFailure(call: Call<List<Task>>, t: Throwable) {
+                Log.v("GET ALL TASKS API ERROR", t.toString())
+            }
+        })
+
+        return listOfAllTasks
+    }
+
+    fun getTaskById(id: Int): MutableLiveData<Task> {
+        val listOfAllTasks = MutableLiveData<Task>()
+        val call = service.getTaskById(id)
+
+        call.enqueue(object : Callback<Task> {
+            override fun onResponse(call: Call<Task>, response: Response<Task>) {
+                if (response.isSuccessful) {
+                    listOfAllTasks.value = response.body()!!
+                }
+            }
+            override fun onFailure(call: Call<Task>, t: Throwable) {
+                Log.v("GET TASK BY ID ERROR", t.toString())
+            }
+        })
+
+        return listOfAllTasks
+    }
+
+    fun getATasksByAuthor(authorLogin: String): MutableLiveData<List<Task>> {
+        val listOfAllTasks = MutableLiveData<List<Task>>()
+        val call = service.getTasksByAuthor(authorLogin)
+
+        call.enqueue(object : Callback<List<Task>> {
+            override fun onResponse(call: Call<List<Task>>, response: Response<List<Task>>) {
+                if (response.isSuccessful) {
+                    listOfAllTasks.value = response.body()!!
+                }
+            }
+            override fun onFailure(call: Call<List<Task>>, t: Throwable) {
+                Log.v("TASKS BY AUTHOR ERROR", t.toString())
+            }
+        })
+
+        return listOfAllTasks
+    }
+
+    fun getATasksByReceiver(authorLogin: String): MutableLiveData<List<Task>> {
+        val listOfAllTasks = MutableLiveData<List<Task>>()
+        val call = service.getTasksByReceiver(authorLogin)
+
+        call.enqueue(object : Callback<List<Task>> {
+            override fun onResponse(call: Call<List<Task>>, response: Response<List<Task>>) {
+                if (response.isSuccessful) {
+                    listOfAllTasks.value = response.body()!!
+                }
+            }
+            override fun onFailure(call: Call<List<Task>>, t: Throwable) {
+                Log.v("TASKS BY RECEIVER ERROR", t.toString())
+            }
+        })
+
+        return listOfAllTasks
+    }
+
+    fun getATasksByTitle(fragTitle: String): MutableLiveData<List<Task>> {
+        val listOfAllTasks = MutableLiveData<List<Task>>()
+        val call = service.getTasksByTitle(fragTitle)
+
+        call.enqueue(object : Callback<List<Task>> {
+            override fun onResponse(call: Call<List<Task>>, response: Response<List<Task>>) {
+                if (response.isSuccessful) {
+                    listOfAllTasks.value = response.body()!!
+                }
+            }
+            override fun onFailure(call: Call<List<Task>>, t: Throwable) {
+                Log.v("CREATE TASK API ERROR", t.toString())
+            }
+        })
+
+        return listOfAllTasks
+    }
+
+    fun createTask(newTask: Task): MutableLiveData<String> {
+        val status = MutableLiveData<String>()
+        val call = service.createTask(newTask)
+
+        call.enqueue(object : Callback<String> {
+            override fun onResponse(call: Call<String>, response: Response<String>) {
+                if (response.isSuccessful) {
+                    status.value = response.body()!!
+                }
+            }
+            override fun onFailure(call: Call<String>, t: Throwable) {
+                Log.v("TASKS BY TITLE ERROR", t.toString())
+            }
+        })
+
+        return status
+    }
+
+    fun updateTask(updateTask: Task): MutableLiveData<String> {
+        val status = MutableLiveData<String>()
+        val call = service.updateTask(updateTask)
+
+        call.enqueue(object : Callback<String> {
+            override fun onResponse(call: Call<String>, response: Response<String>) {
+                if (response.isSuccessful) {
+                    status.value = response.body()!!
+                }
+            }
+            override fun onFailure(call: Call<String>, t: Throwable) {
+                Log.v("UPDATE TASK API ERROR", t.toString())
+            }
+        })
+
+        return status
+    }
+
+    fun deleteTask(deleteTask: Task): MutableLiveData<String> {
+        val status = MutableLiveData<String>()
+        val call = service.deleteTask(deleteTask)
+
+        call.enqueue(object : Callback<String> {
+            override fun onResponse(call: Call<String>, response: Response<String>) {
+                if (response.isSuccessful) {
+                    status.value = response.body()!!
+                }
+            }
+            override fun onFailure(call: Call<String>, t: Throwable) {
+                Log.v("DELETE TASK API ERROR", t.toString())
+            }
+        })
+
+        return status
+    }
+}
---
+++ app/src/main/java/com/example/todolist/model/dal/TaskAPI/TaskService.kt
@@ -0,0 +1,35 @@
+package com.example.todolist.model.dal.TaskAPI
+
+import com.example.todolist.model.Task
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Path
+
+interface TaskService {
+    @GET("tasks/all")
+    fun getAllTasks(): Call<List<Task>>
+
+    @GET("/tasks/id/{id}")
+    fun getTaskById(@Path("id") id: Int): Call<Task>
+
+    @GET("/tasks/author/{login}")
+    fun getTasksByAuthor(@Path("login") login: String): Call<List<Task>>
+
+    @GET("/tasks/receiver/{login}")
+    fun getTasksByReceiver(@Path("login") login: String): Call<List<Task>>
+
+    @GET("/tasks/name/{fragTitle}")
+    fun getTasksByTitle(@Path("fragTitle") fragTitle: String): Call<List<Task>>
+
+    @POST("/tasks/add")
+    fun createTask(@Body task: Task): Call<String>
+
+    @POST("/tasks/delete")
+    fun deleteTask(@Body task: Task): Call<String>
+
+    @POST("/tasks/update")
+    fun updateTask(@Body task: Task): Call<String>
+
+}
